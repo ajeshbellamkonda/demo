8db052e5bfb3f79cd0041558d0d24bacacf82a33
ajesh <ajeshkb90@gmail.com>
2020-10-27 12:59:59 -0400

    created main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-testing
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "***"
+  secret_key = "***"
+  }
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+}
